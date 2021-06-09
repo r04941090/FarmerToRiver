20fbc78db984d7fa25da6b5001ce1d8bd2ff8027
Kevin <apomi0516@gmail.com>
2021-06-09 16:54:08 +0800

    原檔上傳

---
+++ 農夫過河/Data.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace 農夫過河
+{
+    class backDatas
+    {
+        public List<string> back_left { set; get; }
+        public List<string> back_right { set; get; }
+    }
+    class Data
+    {
+        public List<string> list_Left { set; get; }
+        public List<string> list_Right { set; get; }
+        private List<backDatas> backDatas;
+        private bool Direction;
+
+        public Data()
+        {
+            string[] raw = new[]{ "農夫", "菜", "狼", "羊"};
+            list_Left = new List<string>(raw);
+            list_Right = new List<string>();
+            backDatas = new List<backDatas> { new backDatas() { back_left = new List<string>(list_Left.ToArray()),
+                                                                back_right= new List<string>()} };
+            // 後面直接存list_Left、list_Right，因為共用同一個記憶體空間所以後續存的資料都會一樣
+            Direction = true;
+        }
+        public bool Move(string item)
+        {
+            //  三元條件運算子
+            List<string> AddList = (Direction ? list_Right : list_Left);
+            List<string> RemoveList = (Direction ? list_Left : list_Right);
+
+            if (RemoveList.Contains("農夫"))
+            {
+                if (item != "農夫")
+                {
+                    RemoveList.Remove(item);
+                    AddList.Add(item);
+                }
+                RemoveList.Remove("農夫");
+                AddList.Add("農夫");
+                return true;
+                // 移動成功
+            }
+            else
+            {
+                return false;
+                // 移動失敗
+            }
+        }
+        public string Check()
+        {
+            List<string> Checklist;
+
+            if (list_Left.Count != 0)
+            //  三元條件運算子
+            // https://docs.microsoft.com/zh-tw/dotnet/csharp/language-reference/operators/conditional-operator
+            {
+                Checklist = (!list_Left.Any((x) => x == "農夫") ? list_Left : list_Right);
+                return (((Checklist.Any((x) => x == "羊") && Checklist.Any((x) => x == "菜")) ||
+                         (Checklist.Any((x) => x == "羊") && Checklist.Any((x) => x == "狼"))) ? "Fail" : "Pass");
+            }
+            else
+            {
+                return "Finish";
+            }
+        }
+        public bool Back()
+        {
+            if (backDatas.Count > 1)
+            {
+                list_Left = new List<string>(backDatas[backDatas.Count - 2].back_left);
+                list_Right = new List<string>(backDatas[backDatas.Count - 2].back_right);
+                backDatas.RemoveAt(backDatas.Count - 1);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void AddBackData()
+        {
+            backDatas.Add(new backDatas(){back_left = new List<string>(list_Left.ToArray()),
+                                          back_right = new List<string>(list_Right.ToArray())});
+            // 建構式：list<T>(Array) 
+        }
+        public void ChangeDirection(bool Direction)
+        {
+            this.Direction = Direction;
+        }
+
+    }
+}
---
+++ 農夫過河/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 農夫過河
+{
+    
+    public partial class Form1 : Form
+    {
+        private Data data;
+        private ListBox ListBox;
+        public Form1()
+        {
+            
+            InitializeComponent();
+            SetListMode();
+            Intialize();
+            
+        }
+        private void Intialize()
+        {
+            InitializeData();
+            ChangeData();
+            IntializeForm();
+        }
+        private void InitializeData()
+        {
+            data = new Data();
+        }
+        private void IntializeForm()
+        {
+            ChangeButtonState(false);
+            button1.Text = "-->";
+            ListBox = listBox1;
+            ListBox.SelectedIndex = 0;
+        }
+        private void SetListMode()
+        {
+            listBox1.SelectionMode = SelectionMode.One;
+            listBox2.SelectionMode = SelectionMode.One;
+        }
+        private void ChangeData()
+        {
+            listBox1.DataSource = null;
+            listBox2.DataSource = null;
+            listBox1.DataSource = data.list_Left;
+            listBox2.DataSource = data.list_Right;
+        }
+        private void ChangeButtonState(bool Enable)
+        {
+            button2.Enabled = Enable;
+            button4.Enabled = Enable;
+        }
+        /// <summary>
+        /// 更新ListBox的hightlight狀態，並且顯示在目前移動的項目上
+        /// </summary>
+        /// <param name="item"></param>
+        private void RefreshListBox (string item)
+        {
+            ListBox.SelectedIndex = -1;
+            if (ListBox == listBox1)
+            {
+                ListBox = listBox2;
+                data.ChangeDirection(false);
+                button1.Text = "<--";
+            }
+            else
+            {
+                ListBox = listBox1;
+                data.ChangeDirection(true);
+                button1.Text = "-->";
+            }
+            ListBox.SelectedItem = item;
+        }
+        /// <summary>
+        /// 用箭頭提醒使用者目前移動的方向
+        /// </summary>
+        /// <param name="Select"></param>
+        /// <param name="UnSelect"></param>
+        private void ListBoxMouseDown(ListBox Select, ListBox UnSelect)
+        {
+            button1.Text = (Select == listBox1 ? "-->" : "<--");
+            if (Select.SelectedItem != null)
+            {
+                UnSelect.SelectedIndex = -1;
+                ListBox = Select;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string item = (string)ListBox.SelectedItem;
+            // 用意在於之後如果item要做修改，只要改變數右邊的判斷式
+            if(data.Move(item))
+            {
+                ChangeData();
+                if (data.Check() == "Fail")
+                {
+                    MessageBox.Show(data.Check());
+                    Intialize();
+                }
+                else if (data.Check() == "Pass")
+                {
+                    ChangeButtonState(true);
+                    RefreshListBox(item);
+                    data.AddBackData();
+                }
+                else if (data.Check() == "Finish")
+                {
+                    if (MessageBox.Show($"{data.Check()}{Environment.NewLine}Reset or not", "Confirm Message", MessageBoxButtons.OKCancel) == DialogResult.OK)
+                    // https://www.cnblogs.com/rainman/archive/2013/06/03/3116283.html
+                    {
+                        Intialize();
+                    }
+                }
+            }
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // Back
+            if (data.Back())
+            {
+                ChangeData();
+                listBox1.SelectedIndex = -1;
+                listBox2.SelectedIndex = -1;
+
+                if (data.list_Right.Count == 0)
+                    // 代表回到最初的狀態
+                {
+                    ChangeButtonState(false);
+                    listBox1.SelectedIndex = 0;
+                    button1.Text = "-->";
+                }
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Reset
+            InitializeData();
+            ChangeData();
+        }
+        private void listBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            data.ChangeDirection(true);
+            ListBoxMouseDown(listBox1, listBox2);
+        }
+        private void listBox2_MouseDown(object sender, MouseEventArgs e)
+        {
+            data.ChangeDirection(false);
+            ListBoxMouseDown(listBox2, listBox1);
+        }
+    }
+}
